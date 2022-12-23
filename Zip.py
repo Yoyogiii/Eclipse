cf32dd638d3ecbb652c8d2dcf32ef62cd59eac38
Yoyogii-7 <j.yogeeswaran.7@gmail.com>
2022-12-23 16:32:10 +0530

    Python

--- Zip.py
+++ Zip.py
@@ -15,3 +15,16 @@ a={1,2,3,4,5,6,7,8,9}
 b={"Apple","Mango","Grapes","Guava","Apple","Mango"}
 c=zip(a,b)
 print(set(c))
+
+BP=[100,200,300,400,500]
+SP=[600,700,700,700,1000]
+P=zip(BP,SP)
+for  BP,SP in P:
+    if BP<SP:
+        x=SP-BP
+        print("Profit",x)
+    elif BP > SP:
+        x = BP-SP
+        print("Profit", x)
+
+
